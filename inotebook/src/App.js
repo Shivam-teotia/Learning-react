2b184b624daa45b612aa1746c611dc57019ae78e
Shivam-Teotia <shivamteotia200@gamil.com>
2022-08-09 13:43:22 +0530

    adding an event listener called onSnapshot so that data get refrefreshed automatically withour refreshing our webist

--- inotebook/src/App.js
+++ inotebook/src/App.js
@@ -37,16 +37,16 @@ class App extends React.Component {
 }};
 
 componentDidMount(){
-  firebase
+  /*firebase
     .firestore()
     .collection('products')
     .get()
     .then((snapshot)=>{
         console.log(snapshot);
 
-        snapshot.docs.map((doc)=>{
-          console.log(doc.data());
-        });
+        //snapshot.docs.map((doc)=>{
+          //console.log(doc.data());
+        //});
 
         const products=snapshot.docs.map((doc)=>{
           const data=doc.data();
@@ -58,7 +58,26 @@ componentDidMount(){
           products:products,
           loading:false
         })
-    })
+    })*/
+    firebase
+      .firestore()
+      .collection('products')
+      .onSnapshot((snapshot)=>{
+        console.log(snapshot);
+        snapshot.docs.map((doc)=>{
+          console.log(doc.data);
+        });
+        const products=snapshot.docs.map((doc)=>{
+          const data = doc.data();
+          data['id']=doc.id;
+          return data;
+        });
+
+        this.setState({
+          products,
+          loading:false
+        })
+      })
 }
 handleIncreaseQuantity=((product)=>{
     console.log('increase');
